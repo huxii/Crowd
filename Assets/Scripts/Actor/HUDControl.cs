858960928302cb401fa3d55a1768aecba46407b3
huxii <huxin37118@gmail.com>
2018-10-14 00:54:16 -0400

    Materials modified.

--- Assets/Scripts/Actor/HUDControl.cs
+++ Assets/Scripts/Actor/HUDControl.cs
@@ -5,33 +5,33 @@ using UnityEngine.UI;
 
 public class HUDControl : MonoBehaviour
 {
-    public GameObject holdProgressBar;
+    //public GameObject holdProgressBar;
 
-    // Use this for initialization
-    void Start()
-    {
-        holdProgressBar.SetActive(false);
-    }
+    //// Use this for initialization
+    //void Start()
+    //{
+    //    holdProgressBar.SetActive(false);
+    //}
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (holdProgressBar.active)
-        {
-            holdProgressBar.GetComponent<Image>().fillAmount += Time.deltaTime;
-        }
-    }
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    if (holdProgressBar.active)
+    //    {
+    //        holdProgressBar.GetComponent<Image>().fillAmount += Time.deltaTime;
+    //    }
+    //}
 
-    public void CreateHoldIcon(GameObject obj, Vector3 offset)
-    {
-        holdProgressBar.SetActive(true);
-        holdProgressBar.transform.position = obj.transform.position + offset;
-        holdProgressBar.GetComponent<Image>().fillAmount = 0;
-    }
+    //public void CreateHoldIcon(GameObject obj, Vector3 offset)
+    //{
+    //    holdProgressBar.SetActive(true);
+    //    holdProgressBar.transform.position = obj.transform.position + offset;
+    //    holdProgressBar.GetComponent<Image>().fillAmount = 0;
+    //}
 
-    public void DestroyHoldIcon(GameObject obj)
-    {
-        holdProgressBar.SetActive(false);
-        holdProgressBar.GetComponent<Image>().fillAmount = 0;
-    }
+    //public void DestroyHoldIcon(GameObject obj)
+    //{
+    //    holdProgressBar.SetActive(false);
+    //    holdProgressBar.GetComponent<Image>().fillAmount = 0;
+    //}
 }
