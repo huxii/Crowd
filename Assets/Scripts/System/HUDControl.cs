dc4d7eca7a16e97b7f0ebe29a3870802c0bfc364
KeyanZ <kz1065@nyu.edu>
2018-10-15 14:49:03 -0400

    Added Scale for open/close image

--- Assets/Scripts/System/HUDControl.cs
+++ Assets/Scripts/System/HUDControl.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Events;
+using DG.Tweening;
 
 public class HUDControl : MonoBehaviour
 {
     public List<UnityEvent> levelUIEvents;
-
+    public List<GameObject> anchors;
     private int curId = 0;
 
     public void PlayNextUIEvent()
@@ -22,15 +23,43 @@ public class HUDControl : MonoBehaviour
     public void DisplayImage(string anchorImage)
     {
         string[] info = anchorImage.Split(',');
-        GameObject anchor = GameObject.Find(info[0]);
-        Sprite outsideImage = Resources.Load<Sprite>("Sprites/" + info[1]);
-        anchor.GetComponent<Image>().sprite = outsideImage;
+        foreach (GameObject anch in anchors){
+            if(anch.name == info[0])
+            {
+                Sprite outsideImage = Resources.Load<Sprite>("Sprites/" + info[1]);
+                anch.GetComponent<Image>().sprite = outsideImage;
+                anch.transform.localScale = Vector3.zero;
+                anch.SetActive(true);
+                anch.transform.DOScale(Vector3.one, float.Parse(info[2]));
+                break;
+            }
+        }
     }
 
-    private void Start()
+    public void CloseImage(string targetImage)
+    {
+        string[] info = targetImage.Split(',');
+        foreach (GameObject anch in anchors)
+        {
+            if (anch.name == info[0])
+            {
+                anch.transform.DOScale(Vector3.zero, float.Parse(info[1])).OnComplete(() => { anch.SetActive(false); });
+                break;
+            }
+        }
+    }
+
+    void Start()
     {
         Services.hudController.PlayNextUIEvent();
         Services.hudController.PlayNextUIEvent();
-        Services.hudController.PlayNextUIEvent();
+
+    }
+    private void Update()
+    {
+        if (Time.time > 2f)
+        {
+            Services.hudController.PlayNextUIEvent();
+        }
     }
 }
