4d3530577edc0b9f741b9281aa898da4834cb0e4
Unknown <kz1065@nyu.edu>
2018-10-14 22:07:04 -0400

    Fixed audio bugs

--- Assets/Scripts/System/SoundControl.cs
+++ Assets/Scripts/System/SoundControl.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
+
 [System.Serializable]
 public class SoundClip
 {
     public string id = null;
-    public string clipName = null;
+    public AudioClip audioClip = null;
     public bool isLooping;
     public float delay;
     public float fadeInDuration;
@@ -15,10 +16,14 @@ public class SoundClip
     public bool isFalloff = false;
     public bool isStandOut = false;
     public float spatialBlend = 0;
+    public bool uniqueReturn = false;
+    public bool uniqueReplace = false;
+
+    public SoundClip() { }
 }
 
 public class SoundControl : MonoBehaviour {
-    List<SoundClip> clips;
+    public List<SoundClip> clips;
     public int pooledAudioAmount = 10;
     List<GameObject> audioSources;
     public GameObject audioSource;
@@ -27,49 +32,41 @@ public class SoundControl : MonoBehaviour {
     // Use this for initialization
     void Start()
     {
-        TextAsset soundlist = Resources.Load<TextAsset>("Audio List");
-
-        string[] list = soundlist.text.Split(new char[] { '\n' });
-
-        for (int i = 1; i < list.Length; i++)
+        string[] lines = System.IO.File.ReadAllLines(Application.dataPath + "//Resources//Sounds//AudioList.csv");
+        for (int i = 1; i < lines.Length; i++)
         {
-            if (list[i][0] == '1' )
+            SoundClip clip = new SoundClip();
+            if (lines[i][0] == '1')
             {
-                string[] row = list[i].Split(new char[] { ',' });
-                Debug.Log(row[0] + "   " + row[1]);
-                SoundClip clip = new SoundClip();
+                string[] row = lines[i].Split(',');
                 clip.id = row[1];
-                clip.clipName = row[2];
-                bool.TryParse(row[3], out clip.isLooping);
+                Debug.Log(row[2]);
+                clip.audioClip = Resources.Load<AudioClip>("Sounds/" + row[2]);
+                clip.isLooping = row[3] == "1" ? true : false;
                 float.TryParse(row[4], out clip.delay);
                 float.TryParse(row[5], out clip.fadeInDuration);
                 float.TryParse(row[6], out clip.fadeOutDuration);
-                bool.TryParse(row[7], out clip.isFalloff);
-                bool.TryParse(row[8], out clip.isStandOut);
+                clip.isFalloff = row[7] == "1" ? true : false;
+                clip.isStandOut = row[8] == "1" ? true : false;
                 float.TryParse(row[9], out clip.spatialBlend);
-                
+                clip.uniqueReturn = row[10] == "1" ? true : false;
+                clip.uniqueReplace = row[11] == "1" ? true : false;
                 clips.Add(clip);
             }
         }
-
-        foreach (SoundClip clip in clips) {
-            Debug.Log(clip.id);
-            Debug.Log(clip.clipName);
-        }
-
-        /*
+        
         audioSources = new List<GameObject>();
         for (int i = 0; i < pooledAudioAmount; i++)
         {
             GameObject audio = (GameObject)Instantiate(audioSource);
-            //audio.SetActive(false);
+            audio.SetActive(false);
             audioSources.Add(audio);
         }
 
         foreach (SoundClip s in clips)
         {
             soundList.Add(s.id, s);
-        }*/
+        }
 	}
 	
 	// Update is called once per frame
@@ -78,27 +75,33 @@ public class SoundControl : MonoBehaviour {
 
     public void Play(string id)
     {
-        /*for (int i = 0; i < audioSources.Count; i++)
-        {
-            AudioSource source = audioSources[i].GetComponent<AudioSource>();
-            if (!audioSources[i].activeInHierarchy)
-            {
-                //AudioClip clip = Resources.Load<AudioClip>("Sounds/" + soundList[id].clip.name);
-                source.clip = soundList[id].clip;
-                source.loop = soundList[id].isLooping;
-                source.PlayDelayed(soundList[id].delay);
-                source.DOFade(1, soundList[id].fadeInDuration);
-                audioSources[i].SetActive(true);
-                break;
-            }
-        }*/
         for (int i = 0; i < audioSources.Count; i++)
         {
             AudioSource source = audioSources[i].GetComponent<AudioSource>();
+            if (source.clip != null &&  source.clip.name == soundList[id].audioClip.name)
+            {
+                if (soundList[id].uniqueReturn)
+                {
+                    return;
+                }
+                else if (soundList[id].uniqueReplace)
+                {
+                    audioSources[i].SetActive(false);
+                }
+            }
+        }
+        for (int i = 0; i < audioSources.Count; i++)
+        {
+            AudioSource source = audioSources[i].GetComponent<AudioSource>();
+            Debug.Log(audioSources[i]);
             if (!source.isPlaying)
             {
-                AudioClip clip = Resources.Load<AudioClip>("Sounds/" + soundList[id].clipName);
-                source.PlayOneShot(clip);
+                source.loop = soundList[id].isLooping;
+                source.clip = soundList[id].audioClip;
+                audioSources[i].SetActive(true);
+                source.DOFade(1, soundList[id].fadeInDuration);
+                source.PlayDelayed(soundList[id].delay);
+
                 break;
             }
         }
@@ -108,7 +111,8 @@ public class SoundControl : MonoBehaviour {
     {
         foreach (GameObject source in audioSources)
         {
-            if (source.GetComponent<AudioSource>().clip.name == soundList[id].clipName)
+            Debug.Log(source.GetComponent<AudioSource>());
+            if (source.GetComponent<AudioSource>().clip != null && source.GetComponent<AudioSource>().clip.name == soundList[id].audioClip.name)
             {
                 source.GetComponent<AudioSource>().DOFade(0, soundList[id].fadeOutDuration).OnComplete(() => { source.SetActive(false); });
             }
