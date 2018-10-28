6225d321dfba9d2b87166235b75e7ed770648056
huxii <huxin37118@gmail.com>
2018-10-28 12:38:58 -0400

    Flags & ballons animated.

---
+++ Assets/Scripts/Actor/BallonBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallonBehavior : MonoBehaviour
+{
+    public float swingAngle = 5f;
+    public float swingDuration = 1f;
+
+    private bool activated = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!activated)
+        {
+            Services.dotweenEvents.Zigzag(gameObject.name + " y " + swingAngle.ToString() + " " + swingDuration.ToString() + " -1 true");
+            activated = true;
+        }
+    }
+}
