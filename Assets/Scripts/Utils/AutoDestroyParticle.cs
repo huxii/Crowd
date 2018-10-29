9e814e40cfa8f10cce41053a927ade8356a8a229
huxii <huxin37118@gmail.com>
2018-10-29 00:07:04 -0400

    Click vfx replaced.

--- Assets/Scripts/Utils/AutoDestroyParticle.cs
+++ Assets/Scripts/Utils/AutoDestroyParticle.cs
@@ -4,19 +4,28 @@ using UnityEngine;
 
 public class AutoDestroyParticle : MonoBehaviour
 {
+    private float timer = 0;
 
     // Use this for initialization
     void Start()
     {
-
+        timer = GetComponent<ParticleSystem>().duration * 2;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!GetComponent<ParticleSystem>().isPlaying)
+        if (timer > 0)
         {
-            Destroy(gameObject);
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                Destroy(gameObject);
+            }
         }
+        //if (!GetComponent<ParticleSystem>().isPlaying)
+        //{
+        //    Destroy(gameObject);
+        //}
     }
 }
