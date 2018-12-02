9a865e5b2ee8348b7072643121e78518bf57805c
huxii <huxin37118@gmail.com>
2018-12-02 15:55:17 -0500

    Level0 main mechanics completed.

--- Gremlins/Assets/Scripts/Actor/ObjectRandomControl.cs
+++ Gremlins/Assets/Scripts/Actor/ObjectRandomControl.cs
@@ -6,31 +6,57 @@ using UnityEngine.Events;
 public class ObjectRandomControl : ObjectBasicControl
 {
     public List<UnityEvent> randomClickEvents;
+    public bool isAutoPlaying = false;
+
+    private float autoPlayingCD;
+    private float autoPlayingTimer;
 
     // Use this for initialization
     void Start()
     {
-
+        SetAutoPlayingCD();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!IsCoolingDown() && !IsActivated() && !IsLocked())
+        {
+            if (autoPlayingTimer > 0)
+            {
+                autoPlayingTimer -= Time.deltaTime;
+                if (autoPlayingTimer <= 0)
+                {
+                    DoRandomEvent();
+                }
+            }
+        }
+    }
 
+    private void SetAutoPlayingCD()
+    {
+        autoPlayingCD = Random.Range(3, 7);
+        autoPlayingTimer = autoPlayingCD;
+    }
+
+    private void DoRandomEvent()
+    {
+        CoolDown();
+        SetAutoPlayingCD();
+
+        int randomPick = Random.Range(0, randomClickEvents.Count);
+        randomClickEvents[randomPick].Invoke();
     }
 
     public override void Click()
     {
         //base.Click();
 
-        if (!IsCoolingDown() && !IsActivated())
+        if (!IsCoolingDown() && !IsActivated() && !IsLocked())
         {
-            CoolDown();
-
             onClick.Invoke();
 
-            int randomPick = Random.Range(0, randomClickEvents.Count);
-            randomClickEvents[randomPick].Invoke();
+            DoRandomEvent();
         }
     }
 }
