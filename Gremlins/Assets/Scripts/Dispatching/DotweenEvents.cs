a72f4109ae02a7291ac8c2e328d131b9edf01ca0
huxii <huxin37118@gmail.com>
2018-12-13 14:47:57 -0500

    Level0 comic added.

--- Gremlins/Assets/Scripts/Dispatching/DotweenEvents.cs
+++ Gremlins/Assets/Scripts/Dispatching/DotweenEvents.cs
@@ -4,156 +4,9 @@ using UnityEngine;
 using UnityEngine.Events;
 using DG.Tweening;
 
-public class DotweenEvents : MonoBehaviour
+public class DotweenEvents : CustomEvents
 {
-    private string[] paras;
-    private int curParaIdx = 0;
     private Dictionary<GameObject, Sequence> sequenceDict = new Dictionary<GameObject, Sequence>();
-
-    char[] spliters = { ',', ' ' };
-
-    void ParseNewPara(string para)
-    {
-        paras = para.Split(spliters, System.StringSplitOptions.RemoveEmptyEntries);
-        curParaIdx = 0;
-    }
-
-    GameObject ParseGameObject()
-    {
-        return GameObject.Find(paras[curParaIdx++]);
-    }
-
-    Vector3 ParseIncrement(float x = 0, float y = 0, float z = 0)
-    {
-        Vector3 inc = new Vector3(x, y, z);
-        if (paras[curParaIdx] == "x")
-        {
-            ++curParaIdx;
-            inc = new Vector3(float.Parse(paras[curParaIdx++]), y, z);
-        }
-        else
-        if (paras[curParaIdx] == "y")
-        {
-            ++curParaIdx;
-            inc = new Vector3(x, float.Parse(paras[curParaIdx++]), z);
-        }
-        else
-        if (paras[curParaIdx] == "z")
-        {
-            ++curParaIdx;
-            inc = new Vector3(x, y, float.Parse(paras[curParaIdx++]));
-        }
-        else
-        {
-            float nx, ny, nz;
-            nx = float.Parse(paras[curParaIdx++]);
-            ny = float.Parse(paras[curParaIdx++]);
-            nz = float.Parse(paras[curParaIdx++]);
-            inc = new Vector3(nx, ny, nz);
-        }
-
-        return inc;
-    }
-
-    float ParseTime()
-    {
-        return float.Parse(paras[curParaIdx++]);
-    }
-
-    float ParseSpeed()
-    {
-        return float.Parse(paras[curParaIdx++]);
-    }
-
-    int ParseLoop()
-    {
-        if (curParaIdx < paras.Length)
-        {
-            int loop = 1;
-            if (int.TryParse(paras[curParaIdx], out loop))
-            {
-                ++curParaIdx;
-                return loop;
-            }
-            else
-            {
-                return 1;
-            }
-        }
-        else
-        {
-            return 1;
-        }
-    }
-
-    bool ParseIsLocalAxis()
-    {
-        if (curParaIdx < paras.Length)
-        {
-            bool isLocal = false;
-            if (bool.TryParse(paras[curParaIdx], out isLocal))
-            {
-                ++curParaIdx;
-                return isLocal;
-            }
-            else
-            {
-                return false;
-            }
-        }
-        else
-        {
-            return false;
-        }
-    }
-
-    Ease ParseEaseType(Ease easeType = Ease.Unset)
-    {
-        if (curParaIdx < paras.Length)
-        {
-            Ease retEaseType = Ease.Unset;
-            switch (paras[curParaIdx])
-            {
-                case "OutBounce":
-                    retEaseType = Ease.OutBounce;
-                    break;
-                case "InOutSine":
-                    retEaseType = Ease.InOutSine;
-                    break;
-                case "InOutCubic":
-                    retEaseType = Ease.InOutCubic;
-                    break;
-                case "InOutQuad":
-                    retEaseType = Ease.InOutQuad;
-                    break;
-                case "InQuad":
-                    retEaseType = Ease.InQuad;
-                    break;
-                case "OutQuad":
-                    retEaseType = Ease.OutQuad;
-                    break;
-                case "Linear":
-                    retEaseType = Ease.Linear;
-                    break;
-                default:
-                    break;
-            }
-
-            if (retEaseType != Ease.Unset)
-            {
-                ++curParaIdx;
-                return retEaseType;
-            }
-            else
-            {
-                return easeType;
-            }
-        }
-        else
-        {
-            return easeType;
-        }
-    }
 
     public void Rotate(string para)
     {
@@ -550,6 +403,24 @@ public class DotweenEvents : MonoBehaviour
             seq.Append(obj.transform.DOMove(origPos + dir * shockFactors[i], time / loop).SetEase(Ease.Linear));
         }
         seq.Append(obj.transform.DOMove(origPos, time / loop / 2).SetEase(Ease.Linear));
+    }
+
+    public void HideSprite(string para)
+    {
+        ParseNewPara(para);
+
+        GameObject obj = ParseGameObject();
+        obj.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0);
+    }
+
+    public void FadeSprite(string para)
+    {
+        ParseNewPara(para);
+
+        GameObject obj = ParseGameObject();
+        float targetAlpha = ParseAlpha();
+        float time = ParseTime();
+        obj.GetComponent<SpriteRenderer>().DOColor(new Color(1, 1, 1, 1), time);
     }
 
     public void KillSequence(GameObject obj)
