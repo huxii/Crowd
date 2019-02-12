70b335c3f56c3a24522b23235d97aa253758b121
huxii <huxin37118@gmail.com>
2019-02-11 19:12:27 -0500

    Fan animation fixed & inflator wind particle added.

--- Gremlins/Assets/Scripts/Actor/DistinctActors/InflatorControl.cs
+++ Gremlins/Assets/Scripts/Actor/DistinctActors/InflatorControl.cs
@@ -7,11 +7,14 @@ public class InflatorControl : PropAutoLoopControl
     [Header("Inflator Settings")]
     public int inflatorCount;
     private int count;
+    private ParticleSystem windPs;
 
     // Use this for initialization
     void Start()
     {
         ResetCount();
+
+        windPs = transform.Find("Wind").GetComponent<ParticleSystem>();
     }
 
     private void ResetCount()
@@ -29,43 +32,43 @@ public class InflatorControl : PropAutoLoopControl
         if (man0 == null && man1 != null)
         {
             Services.gameEvents.SwitchCrowdState(man1, CrowdControl.CrowdState.INFLATE_ING);
+            windPs.Pause();
         }
         else
         if (man0 != null && man1 == null)
         {
             Services.gameEvents.SwitchCrowdState(man0, CrowdControl.CrowdState.INFLATE_HANDLE);
+            windPs.Play();
         }
         else
         if (man0 != null && man1 != null)
         {
             Services.gameEvents.SwitchCrowdState(man0, CrowdControl.CrowdState.INFLATE_HANDLE);
             Services.gameEvents.SwitchCrowdState(man1, CrowdControl.CrowdState.INFLATE_COMPLETE);
+            windPs.Pause();
+        }
+        else
+        {
+            windPs.Pause();
         }
     }
 
     public void Inflate()
     {
-        if (IsReady())
+        GameObject man = GetSlotMan(1);
+        if (man != null)
         {
-            GameObject man = GetSlotMan(1);
-            if (man != null)
-            {
-                //Services.dotweenEvents.Scale(man.gameObject.name + " 1.1, 1.1, 1.1, 0.5");
-            }
-
-            ++count;
-            if (count >= inflatorCount)
-            {
-                Services.gameEvents.UnboundMan(man);
-                //Services.gameEvents.SwitchCrowdState(man, CrowdControl.CrowdState.INFLATE_FLOAT);
-                Services.gameEvents.MakeFloatMan(man);
-
-                ResetCount();
-            }
+            //Services.dotweenEvents.Scale(man.gameObject.name + " 1.1, 1.1, 1.1, 0.5");
         }
-        else
+
+        ++count;
+        if (count >= inflatorCount)
         {
-            // wind
+            Services.gameEvents.UnboundMan(man);
+            //Services.gameEvents.SwitchCrowdState(man, CrowdControl.CrowdState.INFLATE_FLOAT);
+            Services.gameEvents.MakeFloatMan(man);
+
+            ResetCount();
         }
     }
 
