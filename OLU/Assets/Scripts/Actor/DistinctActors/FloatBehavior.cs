1ca2ed8fa88b8995daee5cccef89275674ef5b8d
huxii <huxin37118@gmail.com>
2019-03-04 13:05:57 -0500

    Level2 wind zone modified.

--- OLU/Assets/Scripts/Actor/DistinctActors/FloatBehavior.cs
+++ OLU/Assets/Scripts/Actor/DistinctActors/FloatBehavior.cs
@@ -4,43 +4,14 @@ using UnityEngine;
 
 public class FloatBehavior : MonoBehaviour
 {
-    [SerializeField]
-    private List<Vector3> paths = new List<Vector3>();
-    [SerializeField]
-    private int idx = 0;
-
-    // Use this for initialization
-    void Start()
-    {
-        paths.Add(new Vector3(2.43f, 9.75f, -2.13f));
-        paths.Add(new Vector3(-1.43f, 9.75f, -2.13f));
-        paths.Add(new Vector3(2.8f, 1.5f, 0.4f));
-        Blowing();
-    }
-
     // Update is called once per frame
     void Update()
     {
     }
 
-    public void Blowing(int stage = 9999999)
+    public void Init(float h)
     {
-        if (idx > stage)
-        {
-            return;
-        }
-
-        if (idx < paths.Count)
-        {
-            Services.dotweenEvents.MoveTo(gameObject.name + " " + paths[idx].x + " " + paths[idx].y + " " + paths[idx].z + " 1 false");
-
-            ++idx;
-            //if (idx >= paths.Count)
-            //{
-            //    GetComponent<CrowdControl>().Unlock();
-            //    Services.dotweenEvents.ScaleTo(gameObject.name + " 1, 1, 1, 0.5 false");
-            //    Destroy(this);
-            //}
-        }
+        Vector3 pos = transform.position;
+        Services.dotweenEvents.MoveTo(gameObject.name + " " + pos.x + " " + (pos.y + h) + " " + pos.z + " 1 false");
     }
 }
