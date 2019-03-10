303083cdb55e37ca4970ab4ddf26a221d5c4f563
huxii <huxin37118@gmail.com>
2019-03-10 11:25:37 -0400

    Rope ready to implement.

--- OLU/Assets/Scripts/Actor/DistinctActors/RopeTitleBehavior.cs
+++ OLU/Assets/Scripts/Actor/DistinctActors/RopeTitleBehavior.cs
@@ -4,56 +4,80 @@ using UnityEngine;
 
 public class RopeTitleBehavior : MonoBehaviour
 {
-    bool simulated = true;
+    public GameObject startAnchor;
+    public GameObject endAnchor;
 
-    public void SetSimulated(bool en)
+    private GameObject firstSegment;
+    private GameObject lastSegment;
+
+    private void Start()
     {
-        if (simulated != en)
+        firstSegment = transform.GetChild(0).gameObject;
+        lastSegment = transform.GetChild(transform.childCount - 1).gameObject;
+
+        firstSegment.GetComponent<Rigidbody2D>().isKinematic = true;
+        lastSegment.GetComponent<Rigidbody2D>().isKinematic = true;
+    }
+
+    public void Update()
+    {
+        if (startAnchor & endAnchor)
         {
-            simulated = en;
-            if (!simulated)
-            {
-                SetRigidbodySimulated(en);
-            }
-            else
-            {
-                Services.taskManager
-                    .Do(new Wait(0.5f))
-                    .Then(new ActionTask(() => SetRigidbodySimulated(en)));
-            }
+            firstSegment.GetComponent<Rigidbody2D>().MovePosition(startAnchor.transform.position);
+            lastSegment.GetComponent<Rigidbody2D>().MovePosition(endAnchor.transform.position);
         }
     }
 
-    public void SetRigidbodySimulated(bool en)
-    {
-        if (simulated == en)
-        {
-            Rigidbody2D[] rbs = GetComponentsInChildren<Rigidbody2D>();
-            foreach (Rigidbody2D rb in rbs)
-            {
-                rb.simulated = en;
-                rb.constraints = en ? RigidbodyConstraints2D.None : RigidbodyConstraints2D.FreezeAll;
-            }
+    //bool simulated = true;
 
-            //if (!en)
-            //{
-            //    GetComponent<Rope>().UpdateRope(GetComponent<Rope>());
-            //}
+    //public void SetSimulated(bool en)
+    //{
+    //    if (simulated != en)
+    //    {
+    //        simulated = en;
+    //        if (!simulated)
+    //        {
+    //            SetRigidbodySimulated(en);
+    //        }
+    //        else
+    //        {
+    //            Services.taskManager
+    //                .Do(new Wait(0.5f))
+    //                .Then(new ActionTask(() => SetRigidbodySimulated(en)));
+    //        }
+    //    }
+    //}
 
-            if (!en)
-            {
-                foreach (Rigidbody2D rb in rbs)
-                {
-                    rb.mass = 0.0001f;
-                }
-            }
-            else
-            {
-                foreach (Rigidbody2D rb in rbs)
-                {
-                    rb.mass = 0.1f;
-                }
-            }
-        }
-    }
+    //public void SetRigidbodySimulated(bool en)
+    //{
+    //    if (simulated == en)
+    //    {
+    //        Rigidbody2D[] rbs = GetComponentsInChildren<Rigidbody2D>();
+    //        foreach (Rigidbody2D rb in rbs)
+    //        {
+    //            rb.simulated = en;
+    //            rb.constraints = en ? RigidbodyConstraints2D.None : RigidbodyConstraints2D.FreezeAll;
+    //        }
+
+    //        //if (!en)
+    //        //{
+    //        //    GetComponent<Rope>().UpdateRope(GetComponent<Rope>());
+    //        //}
+
+    //        if (!en)
+    //        {
+    //            foreach (Rigidbody2D rb in rbs)
+    //            {
+    //                rb.mass = 0.0001f;
+    //            }
+    //        }
+    //        else
+    //        {
+    //            foreach (Rigidbody2D rb in rbs)
+    //            {
+    //                rb.mass = 0.1f;
+    //            }
+    //        }
+    //    }
+    //}
 }
