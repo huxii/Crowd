806b3178103f10c91c48050fc03cd201e0576747
huxii <huxin37118@gmail.com>
2019-04-06 15:57:24 -0400

    Level3 updated.

--- OLU/Assets/Scripts/Actor/DistinctActors/RunesBehavior.cs
+++ OLU/Assets/Scripts/Actor/DistinctActors/RunesBehavior.cs
@@ -4,13 +4,46 @@ using UnityEngine;
 
 public class RunesBehavior : ObjectControl
 {
+    private Vector3 targetRot;
+    private Vector3 curRot;
+    private float speed = 5f;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        targetRot = transform.localEulerAngles;
+        curRot = transform.localEulerAngles;
+    }
+
+    private void Update()
+    {
+        curRot = Vector3.Lerp(curRot, targetRot, Time.deltaTime * speed);
+        transform.localEulerAngles = curRot;
+    }
+
     protected override void RegisterEvents()
     {
         base.RegisterEvents();
+
+        Services.eventManager.Register<CameraAngleEvent>(OnCameraAngleChanged);
     }
 
     protected override void UnregisterEvents()
     {
         base.UnregisterEvents();
+
+        Services.eventManager.Unregister<CameraAngleEvent>(OnCameraAngleChanged);
+    }
+
+    private void OnCameraAngleChanged(Crowd.Event e)
+    {
+        CameraAngleEvent ce = (CameraAngleEvent)e;
+        float y = ce.angle.x;
+        if (y > 180)
+        {
+            y -= 360;
+        }
+        targetRot = new Vector3(0, y, 0);
     }
 }
--- OLU/Assets/Scripts/Actor/DistinctActors/RunnerFirstControl.cs
+++ OLU/Assets/Scripts/Actor/DistinctActors/RunnerFirstControl.cs
@@ -6,14 +6,56 @@ using UnityEngine.Events;
 public class RunnerFirstControl : CycleControl
 {
     private PropAutoBasicControl controlPanel;
+    private int activateCount = 0;
 
     protected void Awake()
     {
         controlPanel = GameObject.Find("ControlPanel0").GetComponent<PropAutoBasicControl>();
     }
 
-    public bool IsShort()
+    private void CheckStairs()
     {
-        return controlPanel.IsActivated();
+        if (curAbsCycle == 1 && IsActivated())
+        {
+            Services.gameEvents.ConnectPath("Tile34, Tile33");
+        }
+        else
+        {
+            Services.gameEvents.DisconnectPath("Tile34, Tile33");
+        }
+    }
+
+    public override void Activate()
+    {
+        ++activateCount;
+        if (activateCount == 1)
+        {
+            base.Activate();
+            CheckStairs();
+        }
+    }
+
+    public override void Deactivate()
+    {
+        --activateCount;
+        if (activateCount == 0)
+        {
+            base.Deactivate();
+            CheckStairs();
+        }
+    }
+
+    public override void GoClockwise()
+    {
+        base.GoClockwise();
+
+        CheckStairs();
+    }
+
+    public override void GoCounterClockwise()
+    {
+        base.GoCounterClockwise();
+
+        CheckStairs();
     }
 }
